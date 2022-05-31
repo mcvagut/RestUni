c242d801c1d10dae8a2cfac1970d0801de5110c7
mcvagut <mcvagut@gmail.com>
2022-05-31 14:32:30 -0400

    Agregando

---
+++ src/main/java/bo/edu/ucb/ing/restuni/restuni/RestuniApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ing.restuni.restuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/ing/restuni/restuni/RestuniApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ing.restuni.restuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
